342fcd368705797da15984d7742d38e112c1a734
orscofield <or.shlomo88@gmail.com>
2017-08-09 22:32:34 +0300

    isSolvable() implemented
    checks whether a board can be solved or not in O(n^2)

--- sliding_puzzle.py
+++ sliding_puzzle.py
@@ -15,7 +15,7 @@ Direction = enum(LEFT='left', RIGHT='right', UP='up', DOWN='down')
 
 
 class Puzzle:
-    'this makes the board'
+    'the puzzle object'
 
     dimension = -1
     board = []
@@ -43,11 +43,13 @@ class Puzzle:
                 if self.board[i][j] == 0:
                     return [i, j]
 
+    # swaps two locations on the board
     def swap(self, i1, j1, i2, j2):
         temp = self.board[i1][j1]
         self.board[i1][j1] = self.board[i2][j2]
         self.board[i2][j2] = temp
 
+    # return a possible legal move for the piece, or None otherwise
     def getMove(self, piece):
         zeroPosition = self.getZeroPosition()
         line = zeroPosition[0]
@@ -67,6 +69,7 @@ class Puzzle:
         ):
             return Direction.LEFT
 
+    # pretty prints the current state of the board
     def printBoard(self):
         for i in range(0, self.dimension):
             for j in range(0, self.dimension):
@@ -74,6 +77,7 @@ class Puzzle:
                 if (j == self.dimension - 1):
                     print "\n\n"
 
+    # moves a piece if it is possible and returns the direction it was moved
     def move(self, piece):
         move = self.getMove(piece)
         if (move is not None):
@@ -91,7 +95,8 @@ class Puzzle:
             self.lastMove = piece
             return move
 
-# BFS - origin of a piece is always [(piece-1)/dimension][(piece-1)%dimension]
+    # BFS - origin of a piece is always
+    # [(piece-1)/dimension][(piece-1)%dimension]
     def isGoal(self):
         for i in range(0, self.dimension):
             for j in range(0, self.dimension):
@@ -103,8 +108,7 @@ class Puzzle:
                         return False
         return True
 
-
-# returns all current allowed moves in an array
+    # returns all current allowed moves in an array
     def allowedMoves(self):
         allowedMoves = []
         for i in range(0, self.dimension):
@@ -114,7 +118,7 @@ class Puzzle:
                     allowedMoves.append(piece)
         return allowedMoves
 
-# copies current puzzle
+    # copies current puzzle
     def getCopy(self):
         newPuzzle = Puzzle(self.dimension)
         for i in range(0, self.dimension):
@@ -124,7 +128,7 @@ class Puzzle:
             newPuzzle.path.append(self.path[i])
         return newPuzzle
 
-# BFS visit function
+    # BFS visit function
     def visit(self):
         children = []
         allowedMoves = self.allowedMoves()
@@ -137,7 +141,7 @@ class Puzzle:
                 children.append(newIns)
         return children
 
-# BFS solution
+    # BFS solution, runtime O(4^n)???
     def solBFS(self):
         counter = 0
         head = self.getCopy()
@@ -152,18 +156,19 @@ class Puzzle:
                 return state.path
             counter += 1
             # Too much moves, I stop the solution because it might take forever
-            if (counter == 20000):
+            if (counter == 1000000):
                 print "counter is ", counter
                 return state.path
             states.extend(state.visit())
-# A* solution
-# using real cost function g() which is the number of moves from the start
-# and using heuristic function h() - cost from current state to solution
+
+    # A* solution
+    # using real cost function g() which is the number of moves from the start
+    # and using heuristic function h() - cost from current state to solution
 
     def g(self):
         return len(self.path)
 
-# heuristic 1 - misplaced tiles - how many tiles are in the wrong position
+    # heuristic 1 - misplaced tiles - how many tiles are in the wrong position
     def h1(self):
         count = 0
         for i in range(0, self.dimension):
@@ -176,12 +181,27 @@ class Puzzle:
                         count += 1
         return count
 
-    def solAStar(self):
+    # heuristic 2 - Manhattan Distance |X1-X2| + |Y1 - Y2|
+    def h2(self):
+        dist = 0
+        for i in range(0, self.dimension):
+            for j in range(0, self.dimension):
+                piece = self.board[i][j]
+                if (piece != 0):
+                    originLine = math.floor((piece - 1) / self.dimension)
+                    originColumn = (piece - 1) % self.dimension
+                    dist += math.fabs(i - originLine) +\
+                        math.fabs(j - originColumn)
+        return dist
+
+    def solAStar(self, heuristic):
         counter = 0
         head = self.getCopy()
         head.path = []
         states = []
         heappush(states, (0, head))
+        print "Initial Board:"
+        states[0][1].printBoard()
         while (len(states) > 0):
             state = heappop(states)
             if (state[1].isGoal() is True):
@@ -190,14 +210,69 @@ class Puzzle:
             children = state[1].visit()
             while (len(children) > 0):
                 child = children.pop(0)
-                f = child.g() + child.h1()
+                if (heuristic is 1):
+                    f = child.g() + child.h1()
+                else:
+                    f = child.g() + child.h2()
                 heappush(states, (f, child))
         # print "counter is: ", counter
         # return heappop(states)
 
-# A test showing a BFS solution
+    # check if current puzzle can be solved -
+    # by determing if the permutation is odd or even
+    # odd - unsolvable, even - solvable
+    # time complexity - O(n^2)
+    def isSolvable(self):
+        table = [x for sublist in self.board for x in sublist]
+        print table
+        counter = 0
+        row = 0
+        blankRow = 0
+        for i in range(0, len(table)):
+            if(i % self.dimension):
+                row += 1
+            if(table[i] == 0):
+                blankRow = row
+                continue
+            for j in range(i+1, len(table)):
+                if(table[i] > table[j] and table[j] != 0):
+                    counter += 1
+        print "Number of inversions: ", counter
+        if (self.dimension %2 == 0):
+            if (blankRow %2 == 0):
+                if (counter % 2 == 0):
+                    print "can be solved!"
+                else:
+                    print "can not be solved"
+            elif (counter % 2 != 0):
+                print "can be solved!"
+            else:
+                print "can not be solved"
+        elif (counter % 2 == 0):
+            print "can be solved!"
+        else:
+            print "can not be solved"
+
+# A test showing a BFS solution (both BFS and A* return the same solution)
 test1 = Puzzle(4)
 test1.board = [[1, 0, 3, 4], [5, 2, 6, 8], [9, 10, 7, 12], [13, 14, 11, 15]]
+
+test2 = Puzzle(5)
+test2.board = [[1, 2, 3, 4, 5],
+               [6, 7, 8, 9, 10],
+               [11, 12, 13, 14, 15],
+               [16, 0, 23, 18, 20],
+               [21, 17, 22, 19, 24]]
+
+test3 = Puzzle(4)
+test3.board = [[1, 2, 3, 4], [12, 5, 6, 7], [8, 9, 13, 15], [0, 14, 10, 11]]
+
+test4 = Puzzle(4)
+test4.board = [[6, 1, 3, 7], [2, 9, 8, 4], [5, 10, 0, 12], [13, 14, 11, 15]]
+
+# this one can not be solved - lloyd's riddle
+test5 = Puzzle(4)
+test5.board = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 15, 14, 0]]
 
 # An indication that everything is interpeted
 print "All done!"
